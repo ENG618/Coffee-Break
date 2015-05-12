79a6e2993f5322208797f452ba10daa0e3b1e30a
eng618 <eng618@fullsail.edu>
2015-05-11 22:26:02 -0400

    Centered map on default location.

--- Coffee Break/CoffeeMapViewController.swift
+++ Coffee Break/CoffeeMapViewController.swift
@@ -11,22 +11,51 @@ import MapKit
 
 class CoffeeMapViewController: UIViewController {
     
+    let initialLocation = CLLocation(latitude: 28.538942, longitude: -81.381453)
+    let regionRadius: CLLocationDistance = 1000
+    
+    
     @IBOutlet var mapView: MKMapView!
+    
+    // MARK: Location Manager
+    var locationManager = CLLocationManager()
+    func checkLocationAuthorizationStatus() {
+        if CLLocationManager.authorizationStatus() == .AuthorizedWhenInUse {
+            mapView.showsUserLocation = true
+        } else {
+            locationManager.requestWhenInUseAuthorization()
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        checkLocationAuthorizationStatus()
+        centerMapOnLocation(initialLocation)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    override func viewDidAppear(animated: Bool) {
+        locationManager.delegate = self
+        // Set accuracy
+        locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
+        // Set distance filter
+        locationManager.distanceFilter = 100
+        // Start updating location
+        locationManager.startUpdatingLocation()
     }
-
 
 }
 
+// MARK: Class Helpers
 extension CoffeeMapViewController {
+    
+    func centerMapOnLocation(location: CLLocation) {
+        let coordinateRegion = MKCoordinateRegionMakeWithDistance(location.coordinate, regionRadius * 2.0, regionRadius * 2.0)
+        mapView.setRegion(coordinateRegion, animated: true)
+    }
+}
+
+// MARK: CLLocationManagerDelegate
+extension CoffeeMapViewController: CLLocationManagerDelegate {
     
     
     
