5e5be1c69f8c40e89ffb9547f3ba61b7a354e57d
eng618 <eng618@fullsail.edu>
2015-05-11 20:24:27 -0400

    Initial Commit

---
+++ Coffee Break/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Coffee Break
+//
+//  Created by Eric Garcia on 5/11/15.
+//  Copyright (c) 2015 Garcia Enterprise. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
