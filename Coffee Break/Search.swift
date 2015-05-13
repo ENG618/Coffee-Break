d4f024985fec6f2f93245dc6a4ad344dabe6b91e
eng618 <eng618@fullsail.edu>
2015-05-12 22:48:56 -0400

    Updated formate of search string.

--- Coffee Break/Search.swift
+++ Coffee Break/Search.swift
@@ -13,9 +13,13 @@ class Search {
     private let ClientID = "OLXCMXPWEIJ4JXBCWMD5JIPMXQGX02S15LHZHIHZMRIXCFG2"
     private let ClientSecret = "0CBC51OH2ZVGSZYWYDJ5WSUWLLSIH5AHRDZUNYJRBCCTZNKN"
     
+    var lat:String!
+    var long:String!
+    
     // TODO: Create search methods
     
     private func search() {
-        var searchUrlString = "http://api.foursquare.com/v2/venues/search?client_id=\(ClientID)&client_secret=\(ClientSecret)&radius=16093&query=coffee&openNow=1"
+        var searchUrlString = "http://api.foursquare.com/v2/venues/search?client_id=\(ClientID)&client_secret=\(ClientSecret)&ll=\(lat!),\(long!)&radius=16093&query=coffee&openNow=1"
+        
     }
 }
